9d9e8621552037488ebb4142c677aba887fb455d
Jeremias Pino <jerpino@gmail.com>
2022-01-21 20:47:07 -0300

    Solve simulation and some bounds problems

--- src/motion_planning/motion_planning/fivebar.py
+++ src/motion_planning/motion_planning/fivebar.py
@@ -73,8 +73,8 @@ class FiveBar(object):
                                  np.deg2rad(220)],
                                 [np.deg2rad(-40),
                                  np.deg2rad(220)], [0, 0.5]])
-        self.minDistalAngle = np.deg2rad(5)
-        self.minProximalAngle = np.deg2rad(5)
+        self.minDistalAngle = np.deg2rad(1)
+        self.minProximalAngle = np.deg2rad(1)
 
     def ikine(self, pose: np.ndarray = 0) -> np.ndarray:
         """ Inverse kinematics. If non parameter is passed the current
@@ -91,9 +91,9 @@ class FiveBar(object):
         """
         joints = np.zeros(3)
         if isinstance(pose, int):
-            p = self.endPose[:2]
+            p = np.copy(self.endPose[:2])
             # z axis is equal to q[2]
-            joints[-1] = self.endPose[-1].item()
+            joints[-1] = np.copy(self.endPose[-1].item())
         else:
             p = pose[:2]
             joints[-1] = pose[-1].item()
@@ -112,20 +112,22 @@ class FiveBar(object):
                 # Update robot
                 joints[i] = np.arctan2(A[1], A[0])
             else:
-                # TODO:Implementar alguna forma para que no se bloquee.
                 # TODO:Throw exception
-                print('Point outside of workspace')
-                joints[:2] = self.joints[:2]
+                print(f'Point outside of workspace {pose}')
+                RuntimeError(f"Point outside of workspace {pose}")
+                exit()
         self.arms[0].rOA = self.arms[0].base + self.arms[0].links[0] * \
             np.array([np.cos(joints[0]), np.sin(joints[0])])
         self.arms[1].rOA = self.arms[1].base + self.arms[1].links[0] * \
             np.array([np.cos(joints[1]), np.sin(joints[1])])
-        self.joints = joints
         if self.is_inside_bounds(joints[0], joints[1]):
+            self.joints = joints
+            self.endPose = np.block([p, joints[-1]])
             return joints
         else:
-            print("Out of bounds")
-            return joints
+            print(f"Pose out of bounds {pose}")
+            RuntimeError(f"Pose out of bounds {pose}")
+            exit()
 
     def fkine(self, joint: np.ndarray = 0) -> np.ndarray:
         """ Forward kinematics
@@ -162,17 +164,70 @@ class FiveBar(object):
 
             p = np.append(p, q[-1])
             # Update robot
-            self.endPose = p
             self.arms[0].rOA = rOA1
             self.arms[1].rOA = rOA2
             if self.is_inside_bounds(q[0], q[1]):
+                self.endPose = p
                 return p
             else:
-                print("Out of bounds")
-                return np.zeros((1, 3))
+                print(f"Joints out of bounds: {np.rad2deg(q)}")
+                RuntimeError(f"Joints out of bounds: {np.rad2deg(q)}")
+                exit()
         else:
-            print('Imposible to solve forward kinematic')
-            return np.zeros((1, 3))
+            print(f'Imposible to solve forward kinematic for joints: {q}')
+            exit()
+
+    def are_distals_ok(self):
+        # end2r1 = self.arms[0].rOA - self.endPose[:2]
+        # end2r1 /= np.linalg.norm(end2r1)
+        # end2r2 = self.arms[1].rOA - self.endPose[:2]
+        # end2r2 /= np.linalg.norm(end2r2)
+        # dot_product = np.dot(end2r1, end2r2)
+        # return abs(np.arccos(dot_product)) > self.minDistalAngle
+        # TODO: SOlve problems with this limit
+        return True
+
+    def are_proximals_ok(self, q1, q2):
+        base2r1 = self.arms[0].rOA - self.arms[0].base[:2]
+        base2r1 /= np.linalg.norm(base2r1)
+        base2r2 = self.arms[1].rOA - self.arms[1].base[:2]
+        base2r2 /= np.linalg.norm(base2r2)
+        dot_product = np.dot(base2r2, base2r1)
+        if dot_product == 0:
+            angle = np.pi / 2
+        else:
+            angle = abs(np.arccos(dot_product))
+        if q1 > q2:
+            if angle > self.minProximalAngle:
+                return True
+            else:
+                return False
+        elif q1 < -np.pi / 2:
+            if q1 % (2 * np.pi) > q2:
+                if angle > self.minProximalAngle:
+                    return True
+                else:
+                    return False
+        else:
+            return False
+
+    def are_inside_limits(self, q1, q2):
+        q1 = q1 % (2 * np.pi)
+        q2 = q2 % (2 * np.pi)
+        return q1 >= 2 * np.pi + self.jlimit[0, 0] or q1 <= self.jlimit[
+            0, 1] and q2 >= 2 * np.pi + self.jlimit[1, 0] or q2 <= self.jlimit[
+                1, 1]
+
+    def is_inside_bounds(self, q1, q2):
+        if not self.are_distals_ok():
+            print("distal")
+        if not self.are_proximals_ok(q1, q2):
+            print("proximal")
+        if not self.are_inside_limits(q1, q2):
+            print("Limits")
+            print(q1 * 180 / np.pi, q2 * 180 / np.pi)
+        return self.are_distals_ok() and self.are_proximals_ok(
+            q1, q2) and self.are_inside_limits(q1, q2)
 
     def showRobot(self):
         """ Show robot with numpy
@@ -209,43 +264,29 @@ class FiveBar(object):
         plt.axis(0.6 * np.array([-1, 1, -1, 1]))
         plt.grid(True)
 
-    def distals_angle(self):
-        end2r1 = self.arms[0].rOA - self.endPose[:2]
-        end2r1 /= np.linalg.norm(end2r1)
-        end2r2 = self.arms[1].rOA - self.endPose[:2]
-        end2r2 /= np.linalg.norm(end2r2)
-        dot_product = np.dot(end2r1, end2r2)
-        return abs(np.arccos(dot_product))
-
-    def proximals_angle(self):
-        base2r1 = self.arms[0].rOA - self.arms[0].base[:2]
-        base2r1 /= np.linalg.norm(base2r1)
-        base2r2 = self.arms[1].rOA - self.arms[1].base[:2]
-        base2r2 /= np.linalg.norm(base2r2)
-        dot_product = np.dot(base2r2, base2r1)
-        return abs(np.arccos(dot_product))
-
-    def is_inside_bounds(self, q1, q2):
-        return self.distals_angle(
-        ) > self.minDistalAngle and self.proximals_angle(
-        ) > self.minProximalAngle and q2 < q1
-
     def work_space(self):
-        q1 = np.arange(start=self.jlimit[0, 0],
-                       stop=self.jlimit[0, 1] + 0.1,
-                       step=0.1)
+        q1 = np.arange(start=-2 * np.pi, stop=2 * np.pi, step=0.05)
         q2 = np.copy(q1)
-        max_points = 1035
-        p = np.zeros((max_points, 3))
-        q = np.copy(p)
+        max_points = q1.size * q1.size
+        p = np.zeros((2 * max_points, 3))
         count = 0
         for q_2 in q2:
             for q_1 in q1:
                 self.fkine(np.array([q_1, q_2, 0.]))
                 if self.is_inside_bounds(q_1, q_2):
-                    p[count, :] = np.copy(
-                        self.endPose)  # self.fkine(np.array([q_1, q_2, 0.]))
-                    q[count, :] = np.array([q_1, q_2, 0.])
+                    p[count, :] = np.copy(self.endPose)
+                    # self.showRobot()
+                    # filenumber = count
+                    # filenumber = format(filenumber, "05")
+                    # filename = "image{}.png".format(filenumber)
+                    # plt.savefig(filename)
+                    # plt.close()
+                    count += 1
+        for q_1 in q1:
+            for q_2 in q2:
+                self.fkine(np.array([q_1, q_2, 0.]))
+                if self.is_inside_bounds(q_1, q_2):
+                    p[count, :] = np.copy(self.endPose)
                     # self.showRobot()
                     # filenumber = count
                     # filenumber = format(filenumber, "05")
@@ -264,7 +305,7 @@ class FiveBar(object):
         # rs = self.arms[0].links[1] - self.arms[0].links[0]
         # plt.plot(rs * np.cos(q1), rs * np.sin(q1), 'k')
         q3 = np.arange(start=self.jlimit[0, 0],
-                       stop=np.deg2rad(120) + 0.1,
+                       stop=np.deg2rad(125) + 0.1,
                        step=0.1)
         plt.plot(0.35 * np.cos(q3) + 0.23 * np.cos(self.jlimit[0, 0]),
                  0.35 * np.sin(q3) + 0.23 * np.sin(self.jlimit[0, 0]), 'k')
@@ -272,12 +313,9 @@ class FiveBar(object):
                        stop=np.deg2rad(210) + 0.1,
                        step=0.1)
         # Find what kind of curve is.
-        plt.plot(0.355 * np.cos(q4) + 0.23 * np.sin(self.jlimit[0, 1]),
-                 0.355 * np.sin(q4) + 0.23 * np.cos(self.jlimit[0, 1]), 'k')
+        plt.plot(0.35 * np.cos(q4) + 0.23 * np.sin(self.jlimit[0, 1]),
+                 0.35 * np.sin(q4) + 0.23 * np.cos(self.jlimit[0, 1]), 'k')
         plt.grid(True)
-        # plt.figure(2)
-        # plt.plot(q[:count, 0], q[:count, 1], 'b .')
-        # plt.grid(True)
 
 
 def main():
@@ -285,7 +323,15 @@ def main():
     l2 = 0.35
     b = 0.0
     five = FiveBar(np.array([-b, l1, l2]), np.array([b, l1, l2]))
-    five.work_space()
+    q = five.ikine(np.array([0, 0.26, 0]))
+    print(q)
+    p = five.fkine(q)
+    print(p)
+    q1 = five.ikine(p)
+    print(q1)
+    p1 = five.fkine(q1)
+    print(p1)
+
     # [-0.1, 0.2, 0.] [0.0, 0.3, 0.]
     # five.endPose = np.array([0.1, 0.2, 0.])
     # print("-+ mode 1")
@@ -304,7 +350,6 @@ def main():
     # print(five.endPose)
     # plt.subplot(2, 2, 2)
     # five.showRobot()
-    plt.show()
     # print(np.array([[0, 1], [-1, 0]]) @ np.array([1, 2]))
 
 
--- src/motion_planning/motion_planning/task.py
+++ src/motion_planning/motion_planning/task.py
@@ -1,37 +1,57 @@
 import gpath as gp
-import fivebar as fb
 import numpy as np
 import math
 from zmqRemoteApi import RemoteAPIClient
 import time
 import matplotlib.pyplot as plt
 
-d1 = np.array([-0.125, 0.2, 0.2])
-d2 = np.array([0.125, 0.2, 0.2])
-robot = fb.FiveBar(d1, d2)
-path = gp.Path(robot)
-st = np.array([-0.1, 0.3, 0.])
-gl = np.array([0.0, 0.35, 0.3])
-pose = np.array([[-.1, 0.3, 0.], [0.1, 0.3, 0.], [.1, 0.3, 0.]])
-robot.ikine(pose[0, :])
-robot.ikine(pose[1, :])
-robot.ikine(pose[2, :])
-max_v = np.array([1, 1])
-max_a = np.array([4, 4])
+path = gp.Path()
+# st = np.array([-0.1, 0.3, 0.])
+# gl = np.array([0.0, 0.35, 0.3])
+# robot.ikine(pose[0, :])
+# robot.ikine(pose[1, :])
+# robot.ikine(pose[2, :])
 # q, qd, qdd, p, pd, pdd = path.line_poly(start=st, goal=gl, mean_v=5)
-q, qd, qdd, p, pd, pdd = path.line(pose=pose, max_v=max_v, max_a=max_a)
+# q, qd, qdd, p, pd, pdd = path.line(pose=pose, max_v=max_v, max_a=max_a)
 # q, qd, qdd, p, pd, pdd = path.go_to_poly(start=st, goal=gl, mean_v=0.5)
-# q, qd, qdd, p, pd, pdd = path.go_to(goals=pose, max_v=max_v, max_a=max_a)
 
 # no resuelve la cinematica inversa
 # plt.plot(q[:, 0], q[:, 1])
-plt.plot(p[:, 0], p[:, 1])
-plt.show()
 client = RemoteAPIClient()
 sim = client.getObject('sim')
+simIK = client.getObject('simIK')
+# Get objects
+base = sim.getObjectHandle('base')
+tip1 = sim.getObjectHandle('tip1')
+target1 = sim.getObjectHandle('target1')
+joint11 = sim.getObjectHandle('q11')
+joint12 = sim.getObjectHandle('q12')
+tip2 = sim.getObjectHandle('tip2')
+target2 = sim.getObjectHandle('target2')
+joint21 = sim.getObjectHandle('q21')
+joint22 = sim.getObjectHandle('q22')
 
-joint1 = sim.getObjectHandle('joint_11')
-joint2 = sim.getObjectHandle('joint_21')
+# Set IK
+ik_env = simIK.createEnvironment()
+# Prepare ik group 1 (q11)
+ik_group1 = simIK.createIkGroup(ik_env)
+simIK.setIkGroupCalculation(ik_env, ik_group1,
+                            simIK.method_damped_least_squares, 0.1, 100)
+ik_element, sim_to_ik_object_map = simIK.addIkElementFromScene(
+    ik_env, ik_group1, base, tip1, target1, simIK.constraint_position)
+simIK.setJointMode(ik_env, sim_to_ik_object_map[joint11],
+                   simIK.jointmode_passive)
+simIK.setJointMode(ik_env, sim_to_ik_object_map[joint12], simIK.jointmode_ik)
+# Second group (q22)
+ik_group2 = simIK.createIkGroup(ik_env)
+simIK.setIkGroupCalculation(ik_env, ik_group2,
+                            simIK.method_damped_least_squares, 0.1, 100)
+ik_element, sim_to_ik_object_map = simIK.addIkElementFromScene(
+    ik_env, ik_group2, base, tip2, target2, simIK.constraint_position)
+simIK.setJointMode(ik_env, sim_to_ik_object_map[joint21],
+                   simIK.jointmode_passive)
+simIK.setJointMode(ik_env, sim_to_ik_object_map[joint22], simIK.jointmode_ik)
+
 # When simulation is not running, ZMQ message handling could be a bit
 # slow, since the idle loop runs at 8 Hz by default. So let's make
 # sure that the idle loop runs at full speed for this program:
@@ -41,16 +61,34 @@ sim.setInt32Param(sim.intparam_idle_fps, 0)
 # Run a simulation in asynchronous mode:
 sim.startSimulation()
 count = 0
-# while (t := sim.getSimulationTime()) < 100:
+
+sim.setJointPosition(joint11, 0)
+sim.setJointPosition(joint21, 0)
+j1 = sim.getJointPosition(joint11) + math.pi - 0.001
+j2 = sim.getJointPosition(joint21)
+start = path.robot.fkine(np.array([j1, j2, 0]))
+# p1 = np.array([-0.4, 0.2, 0.])
+# p2 = np.array([-0.2, 0.4, 0.])
+# p3 = np.array([0.2, 0.4, 0.])
+# p4 = np.array([0.4, 0.2, 0.])
+# pose = np.block([[start], [p1], [p2], [p3], [p4]])
+# max_v = np.array([1, 1, 1, 1])
+# max_a = np.array([4, 4, 4, 4])
+# q, qd, qdd, p, pd, pdd = path.go_to(goals=pose, max_v=max_v, max_a=max_a)
+
+# q, qd, qdd, p, pd, pdd = path.line(pose=pose, max_v=1, max_a=4)
+q, qd, qdd, p, pd, pdd = path.move_y_from_end(.3)
+q[:, 0] = q[:, 0] % (np.pi * 2)
+q1_cop = []
+q2_cop = []
 while count < max(q.shape):
-    # if count % 2 == 0:
-    sim.setJointTargetPosition(joint1, float(q[count, 0] - math.pi / 2))
-    sim.setJointTargetPosition(joint2, float(q[count, 1] - math.pi / 2))
-    # else:
-    #     sim.setJointTargetPosition(joint1, 0 * math.pi / 4)
-    #     sim.setJointTargetPosition(joint2, 0 * math.pi / 4)
-    count += 10
-    time.sleep(0.01)
+    sim.setJointPosition(joint11, float(q[count, 0] - math.pi))
+    sim.setJointPosition(joint21, float(q[count, 1]))
+    simIK.applyIkEnvironmentToScene(ik_env, ik_group1)
+    simIK.applyIkEnvironmentToScene(ik_env, ik_group2)
+    q1_cop.append(sim.getJointPosition(joint11) + math.pi)
+    q2_cop.append(sim.getJointPosition(joint21))
+    count += 1
 sim.stopSimulation()
 # If you need to make sure we really stopped:
 while sim.getSimulationState() != sim.simulation_stopped:
@@ -58,3 +96,10 @@ while sim.getSimulationState() != sim.simulation_stopped:
 
 # Restore the original idle loop frequency:
 sim.setInt32Param(sim.intparam_idle_fps, defaultIdleFps)
+
+plt.figure()
+plt.plot(q[:, 0], 'r')
+plt.plot(q[:, 1], 'g')
+plt.plot(q1_cop, 'k--')
+plt.plot(q2_cop, 'k--')
+plt.show()
