c0a8359c81d3b10cd95cf56c8f18082f79dec7fa
Jeremias Pino <jerpino@gmail.com>
2022-02-19 18:52:17 -0300

    Add encoding trajectory

--- src/motion_planning/motion_planning/serial_communication.py
+++ src/motion_planning/motion_planning/serial_communication.py
@@ -1,20 +1,23 @@
+import time
 import serial
-# import numpy as np
 import pandas as pd
+from os.path import dirname, abspath
 
 
 # TODO: Definir como es la trama [:p q1 q2] o [:p q1 .. q2 ..]
 # TODO: En base a lo anteriror modificar int2hex
 # TODO: Test serial port
-def int2hex(trajectory):
+def encode_trajectory(points):
     tra_hex = ''
-    for point in trajectory:
-        tra_hex += f'{format(point, "04x")}' + ' '
+    for point in points:
+        q1 = f'{format(point[0], "04x")}'
+        q2 = f'{format(point[1], "04x")}'
+        tra_hex += ':P1,' + q1 + '\n' + ':P2,' + q2 + '\n'
     return tra_hex[:-1]
 
 
-def read_csv(path):
-    df = pd.read_csv(path)
+def read_csv(file):
+    df = pd.read_csv(file)
     q = df[['q1', 'q2']].to_numpy()
     z = df['z'].to_numpy()
     time = df['timestamp'].to_numpy()
@@ -22,19 +25,25 @@ def read_csv(path):
 
 
 def main():
-    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
+    ser = serial.Serial(port='/dev/ttyUSB0',
+                        baudrate=115200,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE)
     print(ser.name)  # check which port was really used
-    q, z, t = read_csv(
-        "/home/jere/Documentos/five-bar-robot/src/motion_planning\
-            /trajectories/trajectory.csv")
-    list_encode = int2hex(q[:, 0])
-    list_encode += '\n'
+
+    trj_path = dirname(dirname(abspath(__file__))) + '/trajectories'
+    trj_name = '/trajectory.csv'
+    trajectory = trj_path + trj_name
+    q, _, _ = read_csv(trajectory)
+    list_encode = encode_trajectory(q)
     print(list_encode)
+
     ser.write(list_encode.encode())  # write a string
-    serialString = ser.read(10)
+    time.sleep(1)
+    serialString = ser.readlines()
     # Print the contents of 0he serial data
     print(serialString.decode('Ascii'))
-
     ser.close()  # close port
 
 
